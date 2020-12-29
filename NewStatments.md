2b2cad34cce27d9d8a549403075b71f406ed87d6
Viet Anh Le <mc.vietanh@gmail.com>
2020-12-29 12:09:14 -0500

    done section 5

---
+++ NewStatments.md
@@ -0,0 +1,27 @@
+### Reduce
+```bash
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+```
+
+### every
+
+```bash
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+const array1 = [1, 30, 39, 29, 10, 13];
+
+console.log(array1.every(isBelowThreshold));
+// expected output: true
+
+
+function palindrome(str) {
+  return str.split('').every((char, i) =>{
+    return char === str[str.length - i - 1];
+  })
+}
+```
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n){
+  const reversed = n.toString().split('').reverse().join('');
+  return parseInt(reversed) * Math.sign(n);
+}
+
+
 
 module.exports = reverseInt;
+
+
+
+// function reverseInt(n) {
+//   if(n >= 0){
+//     return parseInt(n.toString().split('').reduce((rev, char) => char + rev, ''));
+//   } else{
+//     const m = n*(-1);
+//     return parseInt(m.toString().split('').reduce((rev, char) => char + rev, '')*(-1));
+//   }
+// }
+
+// function reverseInt(n){
+//   const reversed = n.toString().split('').reverse().join('');
+//   return parseInt(reversed) * Math.sign(n);
+// }
