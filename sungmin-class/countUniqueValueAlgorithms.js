c0a2bbbf779fd7ad7a160a0973fa54a43716c495
Viet Anh Le <mc.vietanh@gmail.com>
2022-06-23 02:58:13 -0400

    done section 5

--- sungmin-class/countUniqueValueAlgorithms.js
+++ sungmin-class/countUniqueValueAlgorithms.js
@@ -5,23 +5,22 @@
 // countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
 // countUniqueValues([]) // 0
 
-// without learning algorithms
-function countUniqueValues(arr) {
+// With learning algorithms
+function countUniqueValueAlgorithms(arr) {
   if (arr.length === 0) {
     return 0;
   }
-  let newArr = [];
-  // work by an array method > push to a new array - this method, we do not use the clue "sorted array"
-  for (let i = 0; i < arr.length; i++) {
-    // if arr[i] doesn't include in newArr => push arr[i] into the newArr
-    if (!newArr.includes(arr[i])) {
-      newArr.push(arr[i]);
+  let i = 0;
+
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      // swapped two element are nearby
+      arr[i] = arr[j];
     }
   }
-
-  return newArr.length;
+  return i + 1;
 }
-
 const testingArr = [
   0, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 6, 6,
   7, 7, 7, 12, 12, 12, 13, 23, 23, 32, 34, 43, 44, 45, 45, 45, 56, 57, 65, 67,
@@ -29,7 +28,9 @@ const testingArr = [
   5675, 6756, 34235, 34534, 6567567,
 ];
 
-var t1 = performance.now();
-countUniqueValues(testingArr);
-var t2 = performance.now();
-console.log(`Call to countUniqueValues took ${(t2 - t1) / 1000} second.`);
+var t3 = performance.now();
+countUniqueValueAlgorithms(testingArr);
+var t4 = performance.now();
+console.log(
+  `Call to countUniqueValueAlgorithms took ${(t4 - t3) / 1000} second.`
+);
